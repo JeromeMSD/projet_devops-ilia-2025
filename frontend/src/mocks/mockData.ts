ed1e36e5505d5a2daaf048fbaa09660f30f8c504
mimounioumnia <Mimouni_Oumnia@etu.u-bourgogne.fr>
2025-11-09 19:10:07 +0100

    affiche la liste des incidents (mock) et fait passer le test (GREEN)

--- frontend/src/mocks/mockData.ts
+++ frontend/src/mocks/mockData.ts
@@ -6,10 +6,14 @@ export interface MockUser {
     role: string;
 }
 
-export const mockUser: MockUser = {
-    id_user: "user-123-abc",
-    firstname: "Sebastien",
-    lastname: "Lacroix",
-    email: "Sebastien.lacroix@projetdevops.com",
-    role: "SRE"
+export const mockUser = {
+    user_id: "user-123-abc",
+    username: "Sebastien Lacroix",
+    email: "seb.lacroix@test.com",
+    role: "sre",
+    created_at: "..."
 };
+export const mockIncidents = [
+    { id: "inc-001", title: "Serveur de connexion inaccessible" },
+    { id: "inc-002", title: "Latence sur l'API des paiements" }
+];
--- frontend/src/routes/ProfilePage.tsx
+++ frontend/src/routes/ProfilePage.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { mockUser } from '../mocks/mockData';
+import { mockUser , mockIncidents } from '../mocks/mockData';
 function ProfilePage() {
     return (
         <div>
-        <h1>Profil de {mockUser.username}</h1> 
+        <h1>Profil de {mockUser.username}</h1>
         <p>{mockUser.email}</p>
         <p>Rôle: {mockUser.role}</p>
+        <hr />
+        <h2>Incidents assignés</h2>
+        <ul>
+            {mockIncidents.map(incident => (
+            <li key={incident.id}>
+                {incident.title}
+            </li>
+            ))}
+        </ul>
         </div>
     );
 }
