e636bceedd3cb1a3c114e389d2e71cb1c243628f
mimounioumnia <Mimouni_Oumnia@etu.u-bourgogne.fr>
2025-11-09 17:14:38 +0100

    le premier test qui échoue ( RED ) pour la page /me

---
+++ frontend/src/routes/ProfilePage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+function ProfilePage() {
+    return (
+        <div>
+            {/* 1e loi devops, le test va échouer */}
+        </div>
+    );
+}
+export default ProfilePage;
