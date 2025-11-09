7e20862c79dd4655a773b82a4a373c11eb724d05
mimounioumnia <Mimouni_Oumnia@etu.u-bourgogne.fr>
2025-11-09 18:09:18 +0100

    ajoute un test (qui échoue) pour l'email et le rôle

--- frontend/src/routes/ProfilePage.test.tsx
+++ frontend/src/routes/ProfilePage.test.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import ProfilePage from './ProfilePage'; 
-import { vi } from 'vitest';
-
 vi.mock('../mocks/mockData', () => ({
     mockUser: {
-        firstname: "Sebastien", 
+        firstname: "Sebastien",
         lastname: "Lacroix",
+        email: "seb.lacroix@test.com", 
+        role: "SRE"
     }
 }));
-
-test(' afficher le nom complet de l\'utilisateur', () => {
+test('afficher le nom complet de l\'utilisateur', () => {
     render(<ProfilePage />);
-    
     const userNameElement = screen.getByText(/Sebastien Lacroix/i); 
-    
     expect(userNameElement).toBeInTheDocument();
+});
+test('afficher l\'email et le rôle de l\'utilisateur', () => {
+    render(<ProfilePage />);
+    const emailElement = screen.getByText(/seb.lacroix@test.com/i);
+    const roleElement = screen.getByText(/Rôle: SRE/i);
+    expect(emailElement).toBeInTheDocument();
+    expect(roleElement).toBeInTheDocument();
 });
