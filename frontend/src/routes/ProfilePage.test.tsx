4fc7f1e936c68470c619740fc36070648a75908e
mimounioumnia <Mimouni_Oumnia@etu.u-bourgogne.fr>
2025-11-09 17:43:35 +0100

    test(profile): ajoute le premier test (qui échoue) pour la page /me

--- frontend/src/routes/ProfilePage.test.tsx
+++ frontend/src/routes/ProfilePage.test.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import ProfilePage from './ProfilePage'; 
-jest.mock('../mocks/mockData', () => ({
+import { vi } from 'vitest';
+vi.mock('../mocks/mockData', () => ({
     mockUser: {
         firstname: "Sebastien",
         lastname: "Lacroix",
