a524b63c9994089151a6923ffad3678b6a50d791
mimounioumnia <Mimouni_Oumnia@etu.u-bourgogne.fr>
2025-11-10 17:12:30 +0100

    ajoute un test (qui échoue) pour le résumé critique

---
+++ frontend/src/routes/DashboardPage.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import DashboardPage from './DashboardPage'; 
+import { vi } from 'vitest';
+
+const mockIncidents = [
+    { id: "inc-001", title: "Serveur critique", severity: "critique" },
+    { id: "inc-003", title: "Fichier critique", severity: "critique" }, 
+    { id: "inc-002", title: "Bug majeur", severity: "majeure" },
+    { id: "inc-004", title: "Problème mineur", severity: "mineure" }
+];
+vi.mock('../mocks/mockData', () => ({
+    mockUser: {
+        username: "Oumnia SRE",
+        email: "oumnia@sre.com",
+        role: "sre"
+    },
+    mockIncidents: mockIncidents 
+}));
+
+test('doit afficher le résumé des incidents critiques (2)', () => {
+    render(<DashboardPage />);
+    const summaryElement = screen.getByText(/2 Incidents Critiques/i);
+
+    expect(summaryElement).toBeInTheDocument();
+});
