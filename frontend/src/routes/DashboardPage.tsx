d1e3f62407fbd64c00d3a18b3df3ea079f5505bd
mimounioumnia <Mimouni_Oumnia@etu.u-bourgogne.fr>
2025-11-10 17:41:17 +0100

    affiche le résumé critique et fait passer le test (GREEN)

--- frontend/src/routes/DashboardPage.tsx
+++ frontend/src/routes/DashboardPage.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { mockIncidents } from '../mocks/mockData';
+import type { MockIncident } from '../mocks/mockData'; 
 function DashboardPage() {
+    const criticalIncidents = mockIncidents.filter(
+        incident => incident.severity.toLowerCase() === 'critique' 
+    );
+    const criticalCount = criticalIncidents.length;
     return (
-        <div>
-        {}
+        <div className="p-4">
+            <h1>Dashboard SRE</h1>
+            <p className="text-xl">
+                {criticalCount} Incidents Critiques en cours
+            </p>
         </div>
     );
 }
