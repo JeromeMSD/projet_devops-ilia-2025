9503e986f41275cb77428b8d9ce47e765caaac79
estelleNdongo <estellendongo02@gmail.com>
2025-11-10 19:36:55 +0100

    la route de deconnexion

--- microservice-user/src/controller.py
+++ microservice-user/src/controller.py
@@ -6,6 +6,7 @@ from .routes.find_all_user import get_all_users_bp
 from .routes.login import login_bp
 from .routes.swagger import swagger_bp
 from .routes.register import register_bp
+from .routes.logout import logout_bp 
 
 load_dotenv()
 BASE_API_URL = os.getenv('BASE_API_URL')
@@ -26,6 +27,8 @@ def register_routes(app: Flask):
     # Routes servant le swagger ui et le swagger.yaml
     app.register_blueprint(swagger_bp)
 
+    #Route pour le déconexion
+    app.register_blueprint(logout_bp, url_prefix=BASE_API_URL)
 
 
 
