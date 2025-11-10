5d3d80a367a63c068f21b63170e09fcff8ba799c
mimounioumnia <Mimouni_Oumnia@etu.u-bourgogne.fr>
2025-11-10 17:58:47 +0100

    affiche la liste filtrée par assignation et fait passer le test (GREEN)

--- frontend/src/routes/DashboardPage.tsx
+++ frontend/src/routes/DashboardPage.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
-import { mockIncidents } from '../mocks/mockData';
+import { mockIncidents , mockUser} from '../mocks/mockData';
 import type { MockIncident } from '../mocks/mockData'; 
 function DashboardPage() {
     const criticalIncidents = mockIncidents.filter(
-        incident => incident.severity.toLowerCase() === 'critique' 
+        (incident: MockIncident) => incident.severity.toLowerCase() === 'critique'
     );
     const criticalCount = criticalIncidents.length;
+    const assignedIncidents = mockIncidents.filter(
+        (incident: MockIncident) => incident.assignee_id === mockUser.user_id
+    );
     return (
         <div className="p-4">
             <h1>Dashboard SRE</h1>
-            <p className="text-xl">
+            {/* Résumé */}
+            <p className="text-xl mb-6">
                 {criticalCount} Incidents Critiques en cours
-            </p>
+            </p>   
+            <hr />
+            {/* Liste filtrée */}
+            <h2>Vos Incidents Assignés ({assignedIncidents.length})</h2>
+            <ul>
+                {assignedIncidents.map((incident: MockIncident) => (
+                    <li key={incident.id}>
+                        {incident.title} - Sévérité: {incident.severity}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
