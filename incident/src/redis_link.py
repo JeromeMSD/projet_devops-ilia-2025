cb78709b98f314371e254743c7d96381c426544c
Augustin <augugu78@gmail.com>
2025-11-12 23:21:27 +0100

    creation d'une fonction permettant de lire un fichier json dans une bdd

--- incident/src/redis_link.py
+++ incident/src/redis_link.py
@@ -1,5 +1,6 @@
 import redis
 import json
+import time
 
 # Connexion à Redis
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)
@@ -8,6 +9,12 @@ try:
     print("✅ Connexion à Redis réussie !")
 except redis.exceptions.ConnectionError as e:
     print(f"❌ Erreur de connexion : {e}")
+
+
+
+
+
+
 
 def saveJSONFile(obj):
     """
@@ -39,11 +46,38 @@ def saveJSONFile(obj):
         print(f"❌ Erreur lors de la sauvegarde : {e}")
         return None
     
-import time
+
+def loadJSONFile(id):
+    """
+    Récupère un objet JSON depuis Redis en utilisant son identifiant.
+    
+    Exemple :
+        loadJSONFile(123) => {"id":123, "title":"Incident", ...}
+    """
+    try:
+        key = f"INC:{id}"
+        
+        # Lecture depuis Redis
+        json_data = r.get(key)
+        
+        if json_data is None:
+            print(f"⚠️ Aucun objet trouvé pour la clé {key}")
+            return None
+        
+        # Conversion JSON -> dict Python
+        obj = json.loads(json_data)
+        return obj
+    
+    except Exception as e:
+        print(f"❌ Erreur lors de la lecture : {e}")
+        return None
+
+
 
 # Création d'un objet JSON exemple
+ide = int(time.time())
 test_incident = {
-    "id": int(time.time()),  # ID unique basé sur le timestamp
+    "id": ide,  # ID unique basé sur le timestamp
     "title": "Latence élevée API",
     "sev": "medium",
     "services": ["api-gateway", "auth-service"],  # Liste de services concernés
@@ -52,5 +86,5 @@ test_incident = {
     "started_at": int(time.time()),  # Timestamp UNIX de création
     "commander": None           # Aucun assigné pour l'instant
 }
-
 saveJSONFile(test_incident)
+print(loadJSONFile(ide))
