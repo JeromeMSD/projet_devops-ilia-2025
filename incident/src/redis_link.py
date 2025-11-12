dc4a1a45f25c02899bff9905cf6ad4e5b0afc16a
Augustin <augugu78@gmail.com>
2025-11-12 23:23:45 +0100

    correction de fautes d'ortographe

--- incident/src/redis_link.py
+++ incident/src/redis_link.py
@@ -6,9 +6,9 @@ import time
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 try:
     r.ping()
-    print("✅ Connexion à Redis réussie !")
+    print("Connexion à Redis réussie !")
 except redis.exceptions.ConnectionError as e:
-    print(f"❌ Erreur de connexion : {e}")
+    print(f"Erreur de connexion : {e}")
 
 
 
@@ -39,11 +39,11 @@ def saveJSONFile(obj):
         # Enregistrement dans Redis
         r.set(key, json_data)
         
-        print(f"✅ Objet enregistré dans Redis avec la clé {key}")
+        print(f"Objet enregistré dans Redis avec la clé {key}")
         return key
     
     except Exception as e:
-        print(f"❌ Erreur lors de la sauvegarde : {e}")
+        print(f"Erreur lors de la sauvegarde : {e}")
         return None
     
 
@@ -61,7 +61,7 @@ def loadJSONFile(id):
         json_data = r.get(key)
         
         if json_data is None:
-            print(f"⚠️ Aucun objet trouvé pour la clé {key}")
+            print(f"Aucun objet trouvé pour la clé {key}")
             return None
         
         # Conversion JSON -> dict Python
