9b4ab3394b76966c5307f51a34f98fcfc3d50eb0
yasminem <yasminemoukhliss@aol.com>
2025-11-14 18:59:04 +0100

    Add test_webhook.py

--- Communication/tests/test_API_routes.py
+++ Communication/tests/test_API_routes.py
@@ -14,17 +14,15 @@ from API_routes import app
 
 @pytest.fixture
 def client():
-    """Crée un client de test pour l'application Flask."""
+    #Crée un client de test pour l'application Flask.
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
 
 
 def test_send_email_route(client):
-    """
-    Test 1 (RED): Vérifie que la route /api/v1/email existe,
-    accepte un POST, et renvoie un succès.
-    """
+    
+    #Test 1 (RED): Vérifie que la route /api/v1/email existe, accepte un POST, et renvoie un succès.
     email_data = {
         "to": "destinataire@example.com",
         "subject": "Sujet du test",
@@ -88,3 +86,14 @@ def test_status_api_format():
     assert isinstance(data["status"], str)
     assert isinstance(data["count"], int)
     assert isinstance(data["data"], list)
+    
+
+def test_register_webhook(client):
+    response = client.post("/api/v1/webhooks", json={
+        "url": "http://example.com",
+        "event_type": "incident"
+    })
+    assert response.status_code == 201
+    data = response.get_json()
+    assert "webhook" in data
+    assert data["webhook"]["url"] == "http://example.com"
