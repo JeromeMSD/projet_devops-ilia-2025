7e5d988836c5ce0e8dfa42524bdbb094025589fb
CyrineElAramouny <131682177+CyrineElAramouny@users.noreply.github.com>
2025-11-14 18:26:52 +0100

    correction de l'API route status pour la lier au redis et vérifier que ça affiche seulement les incidengts récents.
    Updated the get_public_status route to fetch incidents from Redis and filter them based on their status and recency.

--- Communication/src/API_routes.py
+++ Communication/src/API_routes.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from config_redis import redis_client
 from flask import jsonify, Flask, request
 
+
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
@@ -35,35 +36,35 @@ def CreateAnnouncement():
         "announce": announce1
     }),201
 
-
-#données fictives simulées pour tester le get : 
-INCIDENTS = [
-    {
-        "id": 1,
-        "title": "Panne du serveur principal",
-        "status": "en cours",
-        "messages": [
-            {"timestamp": "2025-11-11T09:00:00Z", "text": "Incident détecté."},
-            {"timestamp": "2025-11-11T09:30:00Z", "text": "Équipe en intervention."}
-        ]
-    },
-    {
-        "id": 2,
-        "title": "Maintenance planifiée API",
-        "status": "résolu",
-        "messages": [
-            {"timestamp": "2025-11-10T12:00:00Z", "text": "Maintenance terminée."}
-        ]
-    }
-]
 # route get status
-@app.route("/api/v1/public/status", methods=["GET"])
+@app.route("/api/v1/status", methods=["GET"])
 def get_public_status():
+
+    list_incidents = redis_client.lrange("incidents", 0, -1)
+
+    incidents = []
+    for i in list_incidents:
+        try :
+            incidents.append(json.loads(i))
+        except json.decoder.JSONDecodeError:
+            continue
+
+    incidents_recent = []
+    now = int(time.time())
+
+    for inc in incidents:
+        is_open = inc.get("status") == "open"
+        is_recent = inc.get("started_at", 0) > (now - 86400)  #pour avoir tous les incidents des dernières 24h
+
+        if is_open or is_recent:
+            incidents_recent.append(inc)
+
     return jsonify({
         "status": "success",
-        "count": len(INCIDENTS),
-        "data": INCIDENTS
-    }), 200 
+        "count": len(incidents_recent),
+        "data": incidents_recent
+    }), 200
+
 
 @app.route("/api/v1/email", methods=["POST"])
 def send_email():
@@ -91,6 +92,7 @@ def send_email():
     }), 200 
 
 app.run()
+
 
 
 
